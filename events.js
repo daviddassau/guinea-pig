6c6ee9fc134e1032620cd9c78a05fc3681f2ea64
David Dassau <todieahero@hotmail.com>
2017-08-19 13:07:48 -0500

    completed first three tasks

--- events.js
+++ events.js
@@ -1,1 +1,30 @@
 console.log("I'm in events.js");
+
+
+var outputTarget = document.getElementById('output-target');
+var pageHeader = document.getElementById('page-header');
+
+var showHeaderText = document.getElementById('page-title');
+
+
+function sectionEvents(target){
+	outputTarget.innerHTML = `<p>You clicked on the ${target.innerText}</p>`
+}
+
+document.body.addEventListener('click', function(event){
+	var sectionTarget = event.target;
+	if (event.target.className === 'article-section') {
+		sectionEvents(sectionTarget);
+	}
+});
+
+
+
+showHeaderText.addEventListener('mouseenter', function(){
+	outputTarget.innerHTML = `<p>You moved your mouse over the header</p>`;
+});
+
+
+showHeaderText.addEventListener('mouseleave', function(){
+	outputTarget.innerHTML = `<p>You left me!!</p>`;
+});
